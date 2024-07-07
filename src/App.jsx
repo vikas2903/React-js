8c7466d980ab6927cda89a238827b549d8d791ca
Vikas Prasad <76564198+vikas2903@users.noreply.github.com>
2024-07-07 21:19:03 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import OnclickBgChange from './components/OnclickBgChange'
+
+function App() {
+
+  return (
+    <>
+    <OnclickBgChange /> 
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/OnclickBgChange.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OnclickBgChange() {
+  return (
+   <div className="width-full background-red">vikas</div>
+  )
+} 
+
+export default OnclickBgChange
